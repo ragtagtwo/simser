679abd36dab252f02f5a67ac82ff6c780577a5e6
ragtagtwo <rayenidoudi5@gmail.com>
2023-09-10 18:54:13 +0100

    first comit
    9/10/2023

---
+++ src/main/java/com/mycompany/simser/Simser.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.simser;
+
+/**
+ *
+ * @author rayen
+ */
+public class Simser {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
